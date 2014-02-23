6a27df9e080cefaa14b8e2ae0d3dc308dc46b319
Spencer Krum <krum.spencer@gmail.com>
2014-02-22 17:15:46 -0800

    checkpoint 2

--- prog2.cpp
+++ prog2.cpp
@@ -6,20 +6,51 @@
 //
 
 #include <iostream>
+#include "string.h"
 
 using namespace std;
+bool debug = true;
 
-capitalize_sentences(char * paragraph
-rst:q
+void capitalize_sentences(char * paragraph)
+{
+
+    for (int i = 0; i < strlen(paragraph) ; i++)
+    {
+        //cout << paragraph[i] << endl;
+        if (paragraph[i] == '.' && paragraph[i+1] == ' '){
+            if (debug) {
+                cout << "Found capital candidate: " << paragraph[i+2] << endl;
+            }
+            paragraph[i+2] = toupper(paragraph[i+2]);
+
+        }
+    }
+
+}
+
+void two_spaces_between_sentences(char * paragraph_start, char * paragraph_end)
+{
+
+    for (int i = 0; i < strlen(paragraph_start) ; i++)
+    {
+        paragraph_end[i] = paragraph_start[i];
+    }
+}
 
 
 int main(){
   char paragraph_start[300];
+  char paragraph_end[300];
 
   cout << "Enter paragraph" << endl;
   cin.get(paragraph_start, 300);
   cout << paragraph_start << endl;
 
+  capitalize_sentences(paragraph_start);
+  two_spaces_between_sentences(paragraph_start, paragraph_end);
 
+
+  cout << "Final State:" << endl;
+  cout << paragraph_end << endl;
   return 0;
 }
