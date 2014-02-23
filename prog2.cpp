f9fd77c22a8b21e687d0266cb35b9fa947797700
Spencer Krum <krum.spencer@gmail.com>
2014-02-22 16:20:44 -0800

    early draft

--- prog2.cpp
+++ prog2.cpp
@@ -5,9 +5,20 @@
 // Karla Fant
 //
 
+#include <iostream>
+
+using namespace std;
+
+capitalize_sentences(char * paragraph
+rst:q
 
 
 int main(){
+  char paragraph_start[300];
+
+  cout << "Enter paragraph" << endl;
+  cin.get(paragraph_start, 300);
+  cout << paragraph_start << endl;
 
 
   return 0;
