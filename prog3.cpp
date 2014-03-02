19578450fcc64fb2ce5abb7a92a8f33182c43aba
Spencer Krum <krum.spencer@gmail.com>
2014-03-02 14:08:37 -0800

    prog 3 basics

---
+++ prog3.cpp
@@ -0,0 +1,20 @@
+// CS 162
+// Spencer Krum
+// Assignment 3
+// Karla Fant
+// Winter 2014
+
+
+#include <iostream>
+#include "string.h"
+
+using namespace std;
+const bool debug = true;
+
+
+int main() {
+
+    return 0;
+
+}
+
