12e6433835a0734a27f9e4fad0645a016c0ebfc3
Spencer Krum <krum.spencer@gmail.com>
2014-03-02 14:13:47 -0800

    have the main datatype down

--- prog3.cpp
+++ prog3.cpp
@@ -11,8 +11,19 @@
 using namespace std;
 const bool debug = true;
 
+struct schoolclass { // important not to try to call this 'class'
+    char identifier[20];
+    char name[50];
+    char termyear[20];
+    char comments[100];
+    bool required;
+    char grade;
+};
+
 
 int main() {
+
+    schoolclass classes[60];
 
     return 0;
 
